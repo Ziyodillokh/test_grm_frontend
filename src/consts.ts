8954550b7288d42b36c2df16a0092b09c5be8a1c
Shaxboz <ssamadov883@gmail.com>
2026-05-05 08:10:12 +0500

    i18n(consts): switch MonthsArray labels from Russian to Uzbek

--- src/consts.ts
+++ src/consts.ts
@@ -1,16 +1,16 @@
 export const MonthsArray = [
-  { label: "Январь", value: "1" },
-  { label: "Февраль", value: "2" },
-  { label: "Март", value: "3" },
-  { label: "Апрель", value: "4" },
-  { label: "Май", value: "5" },
-  { label: "Июнь", value: "6" },
-  { label: "Июль", value: "7" },
-  { label: "Август", value: "8" },
-  { label: "Сентябрь", value: "9" },
-  { label: "Октябрь", value: "10" },
-  { label: "Ноябрь", value: "11" },
-  { label: "Декабрь", value: "12" },
+  { label: "Yanvar", value: "1" },
+  { label: "Fevral", value: "2" },
+  { label: "Mart", value: "3" },
+  { label: "Aprel", value: "4" },
+  { label: "May", value: "5" },
+  { label: "Iyun", value: "6" },
+  { label: "Iyul", value: "7" },
+  { label: "Avgust", value: "8" },
+  { label: "Sentabr", value: "9" },
+  { label: "Oktabr", value: "10" },
+  { label: "Noyabr", value: "11" },
+  { label: "Dekabr", value: "12" },
 ];
 
 const startYear = 2024;
